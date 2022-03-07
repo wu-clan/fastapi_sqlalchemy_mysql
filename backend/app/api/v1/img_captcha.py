c7657137f36bd5353119e93b98f399a1ca099af8
wu <jianhengwu0407@gmail.com>
2022-03-07 13:47:00 +0800

    提交验证码接口

---
+++ backend/app/api/v1/img_captcha.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from fast_captcha import iCaptcha
+from fastapi import APIRouter, Request
+from starlette.responses import StreamingResponse
+
+from backend.app.common.sys_redis import redis_client
+from backend.app.core.conf import settings
+from backend.app.utils.generate_uuid import get_uuid
+
+captcha = APIRouter()
+
+
+@captcha.get('/captcha', summary='验证码')
+async def get_captcha(request: Request):
+    img, code = iCaptcha()
+    uid = get_uuid()
+    request.app.state.captcha_uid = uid
+    redis_client.set(uid, code, settings.CAPTCHA_EXPIRATION_TIME)
+    return StreamingResponse(content=img, media_type='image/jpeg')
+
+
+@captcha.post('/captcha', summary='验证码验证')
+async def check_captcha(request: Request):
+    try:
+        code = request.app.state.captcha_uid
+        return {'captcha_uid': code}
+    except AttributeError:
+        return {'msg': '请先获取验证码'}
