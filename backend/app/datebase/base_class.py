61fd6bcaf2adfe94a24d968b3aa1371065e1ded4
wu <jianhengwu0407@gmail.com>
2022-03-07 13:49:40 +0800

    添加 APScheduler

--- backend/app/datebase/base_class.py
+++ backend/app/datebase/base_class.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import uuid
 from typing import Any
 
+from sqlalchemy import Column, Integer
 from sqlalchemy.orm import as_declarative, declared_attr
 
 
 @as_declarative()
 class Base:
-    id: Any
+    # 自动主键id
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     __name__: str
 
     # 自动生成表名
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+
+
+def use_uuid() -> str:
+    """
+    使用uuid
+    :return:
+    """
+    return uuid.uuid4().hex
