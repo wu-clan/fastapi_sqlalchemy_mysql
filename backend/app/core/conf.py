41af73831c701ef5c909b56bdc40774251c1e08f
wu <jianhengwu0407@gmail.com>
2022-03-17 01:40:59 +0800

    更新fastapi描述

--- backend/app/core/conf.py
+++ backend/app/core/conf.py
@@ -13,9 +13,9 @@ class Settings(BaseSettings):
     TITLE: str = 'FastAPI'
     VERSION: str = 'v0.0.1'
     DESCRIPTION: str = """
-    DEMO：fastapi + sqlalchemy(异步) + alembic + aiomysql + aioredis 项目脚手架
+fastapi_mysql_demo. 🚀
     
-    src: https://gitee.com/wu_cl/fastapi_mysql_demo
+### 点击跳转 -> [master](https://gitee.com/wu_cl/fastapi_mysql_demo/tree/master/)
     """
     DOCS_URL: str = '/v1/docs'
     REDOCS_URL: bool = False
