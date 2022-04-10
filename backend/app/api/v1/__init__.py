dc0d6fda2f530b82ee5033a7099c7e7a358723d9
wu <jianhengwu0407@gmail.com>
2022-04-10 18:10:05 +0800

    统一api文件前缀

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter
 
-from .captcha import captcha
-from .test_jobs import aps
-from .test_redis import rd
-from .user import user
+from .v1_captcha import captcha
+from .v1_test_jobs import aps
+from .v1_test_redis import rd
+from .v1_user import user
 
 v1 = APIRouter(prefix='/v1')
 
-v1.include_router(user, prefix='/user', tags=['用户'])
 v1.include_router(captcha, prefix='/captcha', tags=['图片验证码'])
+v1.include_router(user, prefix='/user', tags=['用户'])
 v1.include_router(rd, prefix='/redis', tags=['测试-Redis'])
 v1.include_router(aps, prefix='/job', tags=['测试-APScheduler'])
