3745520ef9167455c2bfcab9f162a4418dae4836
wu <jianhengwu0407@gmail.com>
2022-05-13 16:41:08 +0800

    fix: 数据库异步session垃圾收集器警告

--- backend/app/datebase/db_mysql.py
+++ backend/app/datebase/db_mysql.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
+from contextlib import asynccontextmanager
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -26,12 +27,27 @@ else:
     db_session = sessionmaker(bind=engine, class_=AsyncSession, autoflush=False, expire_on_commit=False)
 
 
-async def get_db() -> AsyncSession:
+@asynccontextmanager
+async def init_db_contextmanager() -> AsyncSession:
+    """
+    初始化数据库上下文管理器
+
+    Ps: https://github.com/sqlalchemy/sqlalchemy/discussions/7164
+    :return:
+    """
+    session = db_session()
+    try:
+        yield session
+    finally:
+        await session.close()
+
+
+async def get_db():
     """
     获取数据库会话
     :return:
     """
-    async with db_session() as session:
+    async with init_db_contextmanager() as session:
         return session
 
 
