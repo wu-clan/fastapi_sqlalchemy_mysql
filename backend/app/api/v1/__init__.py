fdf7430867b1546d6a456da8a9bb245dd9c525b4
wu <jianhengwu0407@gmail.com>
2022-05-16 17:28:49 +0800

    更新api为restful风格

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -8,6 +8,6 @@ from .v1_user import user
 
 v1 = APIRouter(prefix='/v1')
 
-v1.include_router(user, prefix='/user', tags=['用户'])
+v1.include_router(user, tags=['用户'])
+v1.include_router(aps, tags=['测试-APScheduler'])
 v1.include_router(rd, prefix='/redis', tags=['测试-Redis'])
-v1.include_router(aps, prefix='/job', tags=['测试-APScheduler'])
